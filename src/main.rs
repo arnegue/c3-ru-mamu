88d578cade3cd742cfe8e55038c6e642a99e22f7
Arne <arne-@hotmail.de>
2025-10-28 11:41:41 +0100

    Reading SPI working

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::spi::*;
+use esp_idf_hal::units::*;
+
+
+// build: cargo build
+// flash: espflash flash target/riscv32imc-esp-espidf/debug/c3-ru-mamu --monitor
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+    use esp_idf_hal::peripherals::Peripherals;
+
+    let peripherals = Peripherals::take().unwrap();
+    let spi = peripherals.spi2;
+
+    let sclk = peripherals.pins.gpio5;
+    let miso = peripherals.pins.gpio7; // SDI
+    let mosi = peripherals.pins.gpio8; // SDO
+    let cs = peripherals.pins.gpio6;
+
+    println!("Starting SPI loopback test");
+
+    let driver =
+        SpiDriver::new::<SPI2>(spi, sclk, mosi, Some(miso), &SpiDriverConfig::new()).unwrap();
+
+    let config_1 = config::Config::new().baudrate(1.MHz().into());
+    let mut device_1 = SpiDeviceDriver::new(&driver, Some(cs), &config_1).unwrap();
+    let write = false;
+    loop {
+        for register_address in 0u8..0xFu8 {
+            let channel = 0u8;
+            let write_byte = if write { 0x00u8 } else { 0x80u8 };
+            let spi_data: [u8; 2] = [
+                write_byte | ((register_address << 3) | (channel << 1)),
+                0xFFu8,
+            ];
+
+            let mut rx: [u8; 2] = [0u8; 2];
+
+            match device_1.transfer(&mut rx, &spi_data) {
+                Ok(_) => {
+                    log::info!(
+                        "Device 1: Wrote [{:02x}, {:02x}], read [{:02x}, {:02x}]",
+                        spi_data[0],
+                        spi_data[1],
+                        rx[0],
+                        rx[1]
+                    );
+                }
+                Err(_) => {
+                    log::info!("Oh no Error!");
+                }
+            }
+
+            FreeRtos::delay_ms(500);
+        }
+        log::info!("\n");
+    }
+    // Use the `spi` instance as needed
+}
