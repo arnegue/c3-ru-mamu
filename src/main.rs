ee241bf110bd6c06f350f635f9594f9c87dc358a
Arne <arne-@hotmail.de>
2025-10-28 11:51:58 +0100

    Writing registers works

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,32 @@ where
     ret_val
 }
 
+fn write_register<'d, T>(
+    spi_device: &mut SpiDeviceDriver<'d, T>,
+    register_address: u8,
+    channel: u8,
+    value: u8,
+) -> Result<(), EspError>
+where
+    T: Borrow<SpiDriver<'d>> + 'd,
+{
+    let write = true;
+    let write_byte = if write { 0x00u8 } else { 0x80u8 };
+    let spi_data: [u8; 2] = [
+        write_byte | ((register_address << 3) | (channel << 1)),
+        value,
+    ];
+
+    let mut rx: [u8; 0] = [];
+    let ret_val = spi_device.transfer(&mut rx, &spi_data);
+    log::info!(
+        "Device 1: Wrote [{:02x}, {:02x}]]",
+        spi_data[0],
+        spi_data[1]
+    );
+    ret_val
+}
+
 // build: cargo build
 // flash: espflash flash target/riscv32imc-esp-espidf/debug/c3-ru-mamu --monitor
 fn main() {
@@ -59,22 +85,42 @@ fn main() {
     let config_1 = config::Config::new().baudrate(1.MHz().into());
     let mut device_1 = SpiDeviceDriver::new(&driver, Some(cs), &config_1).unwrap();
 
+    let register_address = 0x0A;
+    let channel = 0u8;
+    let mut value = 0;
     loop {
-        for register_address in 0u8..0xFu8 {
-            match read_register(&mut device_1, register_address, 0u8) {
-                Ok(_) => {
-                    log::info!(
-                        "Device 1: Read register address {:02x} successfully",
-                        register_address
-                    );
-                }
-                Err(_) => {
-                    log::info!("Oh no Error!");
-                }
+        match write_register(&mut device_1, register_address, channel, value) {
+            Ok(_) => {
+                log::info!(
+                    "Device 1: Wrote register address {:02x} successfully",
+                    register_address
+                );
             }
-
-            FreeRtos::delay_ms(500);
+            Err(_) => {
+                log::info!("Oh no Error!");
+            }
         }
+        if value == 0 {
+            value = 1;
+        } else {
+            value = 0;
+        }
+        FreeRtos::delay_ms(500);
+        // for register_address in 0u8..0xFu8 {
+        //     match read_register(&mut device_1, register_address, 0u8) {
+        //         Ok(_) => {
+        //             log::info!(
+        //                 "Device 1: Read register address {:02x} successfully",
+        //                 register_address
+        //             );
+        //         }
+        //         Err(_) => {
+        //             log::info!("Oh no Error!");
+        //         }
+        //     }
+
+        //     FreeRtos::delay_ms(500);
+        // }
         log::info!("\n");
     }
 }
