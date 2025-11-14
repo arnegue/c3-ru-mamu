9c477654ee445593d910816f9ce9651858197608
Arne <arne-@hotmail.de>
2025-11-14 14:59:07 +0100

    Interrupt driven reception now working

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-mod sc16is752;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 
 use ::sc16is752::Channel;
-use ::sc16is752::FIFO_SIZE;
+use ::sc16is752::InterruptEventTest;
 use ::sc16is752::Parity;
 use ::sc16is752::PinMode;
 use ::sc16is752::PinState;
@@ -10,8 +11,13 @@ use ::sc16is752::UartConfig;
 use ::sc16is752::GPIO;
 use ::sc16is752::SC16IS752;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::InterruptType;
+use esp_idf_hal::gpio::PinDriver;
+use esp_idf_hal::gpio::Pull;
 use esp_idf_hal::spi::*;
 use esp_idf_hal::units::*;
+
+static INTERUPT_OCCURRED: AtomicBool = AtomicBool::new(false); // Notifier that message transmission is complete
 
 // build: cargo build
 // flash: espflash flash target/riscv32imc-esp-espidf/debug/c3-ru-mamu --monitor
@@ -32,7 +38,7 @@ fn main() {
     let mosi = peripherals.pins.gpio8; // SDO
     let cs = peripherals.pins.gpio3; // CS (old device is pin 6)
 
-    let isr = peripherals.pins.gpio2; // IRQ
+    let isr_pin = peripherals.pins.gpio2; // IRQ
     let reset_pin = peripherals.pins.gpio4; // RST
 
     println!("Starting SPI loopback test");
@@ -44,23 +50,36 @@ fn main() {
     let config_1 = config::Config::new().baudrate(1.MHz().into());
     let device_1 = SpiDeviceDriver::new(&driver, Some(cs), &config_1).unwrap();
 
+    // Interrupt pin for SC16IS752
+    let mut isr_pin_driver = PinDriver::input(isr_pin).unwrap();
+    isr_pin_driver
+        .set_interrupt_type(InterruptType::NegEdge)
+        .unwrap();
+    unsafe { isr_pin_driver.subscribe(gpio_isr).unwrap() }
+    isr_pin_driver.enable_interrupt().unwrap();
+
     // SC16IS752 setup
     let spi_bus = SC16IS752spi::new(device_1);
     let mut sc16is752_a = SC16IS752::new(spi_bus, 1843200.Hz().into());
-
     let device_a_config = UartConfig::new(9600, 8, Parity::NoParity, 1);
-
     sc16is752_a.reset_device();
     sc16is752_a
         .initialise_uart(Channel::A, device_a_config)
         .unwrap();
+    sc16is752_a.ping(); // TODO error handling
     sc16is752_a.gpio_set_pin_mode(GPIO::GPIO0, PinMode::Output);
+    let interrupt_bitmask = 0b1; // 00000101;
+    sc16is752_a.interrupt_control(Channel::A, interrupt_bitmask);
 
+    // Debugging stuff
     let ascii_exclamation_mark = 33;
     let ascii_tilde = 126;
     let mut temp_write_byte: u8 = ascii_exclamation_mark;
     let mut current_led = PinState::High;
+
+    // Main loop
     loop {
+        let mut try_read = false;
         let bool_val = match current_led {
             PinState::High => true,
             PinState::Low => false,
@@ -79,17 +98,55 @@ fn main() {
             false => PinState::High,
         };
 
-        // Read byte
-        let available_bytes = sc16is752_a.fifo_available_data(Channel::A).unwrap();
-        if available_bytes > 0 {
-            match sc16is752_a.read_cycle(Channel::A, available_bytes as usize) {
-                Ok(read_bytes) => {
-                    // There could be a race-condition, that between the call of available bytes and the actual reading the size increases,
-                    // but that shouldn't be that bad and could be handled later when parsing the buffer
-                    let buf_str = buffer_to_string(read_bytes.as_ref(), available_bytes as usize);
-                    log::info!("Device 1: Read {available_bytes} bytes: {buf_str}");
+        if INTERUPT_OCCURRED.load(Ordering::Relaxed) {
+            log::info!("Interrupt occured!");
+            match sc16is752_a.isr(Channel::A) {
+                Ok(interrupt_kind) => match interrupt_kind {
+                    InterruptEventTest::RHR_INTERRUPT => {
+                        log::info!("RHR_INTERRUPT");
+                        try_read = true;
+                    }
+                    InterruptEventTest::RECEIVE_LINE_STATUS_ERROR => {
+                        try_read = true;
+                        log::info!("RECEIVE_LINE_STATUS_ERROR")
+                    }
+                    InterruptEventTest::RECEIVE_TIMEOUT_INTERRUPT => {
+                        try_read = true;
+                        log::info!("RECEIVE_TIMEOUT_INTERRUPT")
+                    }
+                    InterruptEventTest::THR_INTERRUPT => log::info!("THR_INTERRUPT"),
+                    InterruptEventTest::MODEM_INTERRUPT => log::info!("MODEM_INTERRUPT"),
+                    InterruptEventTest::INPUT_PIN_CHANGE_STATE => {
+                        log::info!("INPUT_PIN_CHANGE_STATE")
+                    }
+                    InterruptEventTest::RECEIVE_XOFF => log::info!("RECEIVE_XOFF"),
+                    InterruptEventTest::CTS_RTS_CHANGE => log::info!("CTS_RTS_CHANGE"),
+                    InterruptEventTest::NO_INTERRUPT => log::info!("NO_INTERRUPT"),
+                    InterruptEventTest::UNKNOWN => log::info!("UNKNOWN"),
+                },
+                Err(err) => {
+                    log::error!("Error in isr");
                 }
-                Err(_) => {}
+            }
+            //spi_interrupt_event(sc16is752_a);
+            isr_pin_driver.enable_interrupt().unwrap(); // Reenable interrupt again
+            INTERUPT_OCCURRED.store(false, Ordering::Relaxed);
+        }
+
+        // Interrupt happend which tells, that there might be data in buffer
+        if try_read {
+            let available_bytes = sc16is752_a.fifo_available_data(Channel::A).unwrap();
+            if available_bytes > 0 {
+                match sc16is752_a.read_cycle(Channel::A, available_bytes as usize) {
+                    Ok(read_bytes) => {
+                        // There could be a race-condition, that between the call of available bytes and the actual reading the size increases,
+                        // but that shouldn't be that bad and could be handled later when parsing the buffer
+                        let buf_str =
+                            buffer_to_string(read_bytes.as_ref(), available_bytes as usize);
+                        log::info!("Device 1: Read {available_bytes} bytes: {buf_str}");
+                    }
+                    Err(_) => {}
+                }
             }
         }
 
@@ -120,3 +177,43 @@ fn buffer_to_string(buffer: &[u8], size: usize) -> String {
     }
     result
 }
+
+fn gpio_isr() {
+    INTERUPT_OCCURRED.store(true, Ordering::Relaxed);
+}
+/*
+fn spi_interrupt_event<BUS>(mut device: SC16IS752<BUS>)
+where
+    BUS: ::sc16is752::Bus,
+{
+    match device.interrupt_pending_test(Channel::A) {
+        Ok(_) => {}
+        Err(err) => {
+            log::error!("Error in interrupt_pending_test");
+            // TODO print?
+            return;
+        }
+    }
+
+    match device.isr(Channel::A) {
+        Ok(interrupt_kind) => match interrupt_kind {
+            InterruptEventTest::RHR_INTERRUPT => log::info!("RHR_INTERRUPT"),
+            InterruptEventTest::RECEIVE_LINE_STATUS_ERROR => {
+                log::info!("RECEIVE_LINE_STATUS_ERROR")
+            }
+            InterruptEventTest::RECEIVE_TIMEOUT_INTERRUPT => {
+                log::info!("RECEIVE_TIMEOUT_INTERRUPT")
+            }
+            InterruptEventTest::THR_INTERRUPT => log::info!("THR_INTERRUPT"),
+            InterruptEventTest::MODEM_INTERRUPT => log::info!("MODEM_INTERRUPT"),
+            InterruptEventTest::INPUT_PIN_CHANGE_STATE => log::info!("INPUT_PIN_CHANGE_STATE"),
+            InterruptEventTest::RECEIVE_XOFF => log::info!("RECEIVE_XOFF"),
+            InterruptEventTest::CTS_RTS_CHANGE => log::info!("CTS_RTS_CHANGE"),
+            InterruptEventTest::UNKNOWN => log::info!("UNKNOWN"),
+        },
+        Err(err) => {
+            log::error!("Error in isr");
+        }
+    }
+}
+*/
